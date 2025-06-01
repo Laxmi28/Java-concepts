2b6b34af2ab7c2b0cb3eb16263449a39fc93c68d
Known <laxpandey28@gmail.com>
2025-06-01 22:01:02 +0530

    Revision class additions

--- src/CollectionsframeworkRevision/MapRev1.java
+++ src/CollectionsframeworkRevision/MapRev1.java
@@ -1,7 +1,37 @@
 package CollectionsframeworkRevision;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+
 public class MapRev1 {
 
-    HashMap<String, S>
+   
+   public static void main(String[] args) {
+
+    HashMap<Integer , Integer> map = new HashMap<>();
+            map.put(1,2);
+            map.put(2,3);
+            map.put(null, null);
+            map.put(null,2);
+
+    System.out.println(map);     
+    
+    map.put(9, 0);
+
+    map.putIfAbsent(2, 7);
+
+    System.out.println(map);
+
+    System.out.println(map.getOrDefault(8,null));
+
+    Hashtable<Integer,Integer> table = new Hashtable<>();
+
+    table.put(7, 8);
+    table.putIfAbsent(9, 9);
+
+    System.out.println(table);
+
+    
+   }
 
 }
